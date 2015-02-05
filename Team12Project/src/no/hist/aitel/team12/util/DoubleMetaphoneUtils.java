dc8589b7771a8c790cb0a98e23634295fa9c9abe
helkana <jorgen.skogstad@gmail.com>
2015-02-05 13:54:20 +0100

    Created Double Metaphone Utility class and test class, this will be used for encoding all searchable MySQL fields to Double Metaphone and comparing strings to Double Metaphone values

---
+++ Team12Project/src/no/hist/aitel/team12/util/DoubleMetaphoneUtils.java
@@ -0,0 +1,35 @@
+package no.hist.aitel.team12.util;
+import org.apache.commons.codec.language.DoubleMetaphone;
+/**
+ * 
+ * @author Ole Jørgen
+ *
+ */
+public class DoubleMetaphoneUtils {
+	
+	// Creates a DoubleMetaphone object
+	private static DoubleMetaphone metaphone = new DoubleMetaphone();
+	
+	// No object should be initiated 
+	private DoubleMetaphoneUtils() {}
+	
+	/**
+	 * 
+	 * @param st Input string that is being converted.
+	 * @return Return the encoded string.
+	 */
+	public static String encodeString(String st) {
+		return metaphone.encode(st);
+	}
+	
+	/**
+	 * 
+	 * @param st1 One of the two strings that is being compared
+	 * @param st2 The other of the two strings that is being compared
+	 * @return Returns boolean value for the comparison 
+	 */
+	public static boolean isMetaphoneValueEqual(String st1, String st2) {
+		return metaphone.isDoubleMetaphoneEqual(st1, st2);
+	}
+}
+
---
+++ Team12Project/src/no/hist/aitel/team12/util/test/DoubleMetaphoneUtilsJUnitTest.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package no.hist.aitel.team12.util.test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import no.hist.aitel.team12.util.DoubleMetaphoneUtils;
+
+import org.junit.Test;
+
+/**
+ * @author Ole Jørgen
+ *
+ */
+public class DoubleMetaphoneUtilsJUnitTest {
+
+	@Test
+	public void encodeStringTest() {
+		String s1 = DoubleMetaphoneUtils.encodeString("Trondheim");
+		String s2 = DoubleMetaphoneUtils.encodeString("Merqr");
+		String s3 = DoubleMetaphoneUtils.encodeString("Troondhm");
+		String s4 = DoubleMetaphoneUtils.encodeString("merkur");
+		
+		assertTrue(s2.equals(s4));
+		assertFalse(s4.equals(s3));
+		assertTrue(s1.equals(s3));
+		assertFalse(s1.equals(s2));
+	}
+	
+	@Test
+	public void isMetaphoneValueEqualTest() {
+		String s1 = "Trondheim";
+		String s2 = "Merqr";
+		String s3 = "Troondhm";
+		String s4 = "merkur";
+		
+		assertTrue(DoubleMetaphoneUtils.isMetaphoneValueEqual(s1, s3));
+		assertFalse(DoubleMetaphoneUtils.isMetaphoneValueEqual(s1, s2));
+		assertTrue(DoubleMetaphoneUtils.isMetaphoneValueEqual(s2, s4));
+		assertFalse(DoubleMetaphoneUtils.isMetaphoneValueEqual(s2, s1));
+		
+	}
+
+}
