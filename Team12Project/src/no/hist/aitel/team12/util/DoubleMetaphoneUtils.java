4b5fa37aec3c15df0f101d9624b3aebb5640ddb8
helkana <jorgen.skogstad@gmail.com>
2015-02-05 17:27:29 +0100

    Added comments in DoubleMetaphoneUtils, licence header still missing. Noted in the ToDo list on Google Drive.

--- Team12Project/src/no/hist/aitel/team12/util/DoubleMetaphoneUtils.java
+++ Team12Project/src/no/hist/aitel/team12/util/DoubleMetaphoneUtils.java
@@ -1,20 +1,33 @@
+/*
+ * DoubleMetaphoneUtils
+ * 
+ * v1.0
+ * 
+ * 2015.02.05
+ * 
+ * <Licence info goes here>
+ * 
+ */
+
 package no.hist.aitel.team12.util;
 import org.apache.commons.codec.language.DoubleMetaphone;
 /**
+ * This is a thin wrapper around org.apache.commons.codec.language.DoubleMetaphone.<br />
+ * Read more about the Double Metaphone algorithm on <a href="">Wikipedia</a>.<br />
  * 
  * @author Ole Jorgen
- *
+ * 
  */
 public class DoubleMetaphoneUtils {
 	
 	// Creates a DoubleMetaphone object
 	private static DoubleMetaphone metaphone = new DoubleMetaphone();
 	
-	// No object should be initiated 
+	// Private constructor, no object should be initiated 
 	private DoubleMetaphoneUtils() {}
 	
 	/**
-	 * 
+	 * Converts the input string to a Double Metaphone value.
 	 * @param st Input string that is being converted.
 	 * @return Return the encoded string.
 	 */
@@ -23,7 +36,7 @@ public class DoubleMetaphoneUtils {
 	}
 	
 	/**
-	 * 
+	 * Compares two strings to check if the Double Metaphone values are equal. 
 	 * @param st1 One of the two strings that is being compared
 	 * @param st2 The other of the two strings that is being compared
 	 * @return Returns boolean value for the comparison 
