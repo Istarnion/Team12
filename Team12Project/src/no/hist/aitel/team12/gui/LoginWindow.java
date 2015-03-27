41674948be30bda1a076066c3c712d05aaf19607
Roger Schei Bergheim <roger.schei.bergheim@live.no>
2015-03-27 15:01:32 +0100

    Working with GUI/LoginWindow, added some more languages and began with login-functionality

--- Team12Project/src/no/hist/aitel/team12/gui/LoginWindow.java
+++ Team12Project/src/no/hist/aitel/team12/gui/LoginWindow.java
@@ -4,6 +4,12 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
+import java.beans.Statement;
+import java.security.interfaces.RSAKey;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.swing.*;
 
@@ -12,12 +18,16 @@ import no.hist.aitel.team12.util.PasswordInputField;
 
 import org.omg.CORBA.PUBLIC_MEMBER;
 
-public class LoginWindow extends JFrame implements ActionListener{
+public class LoginWindow extends JFrame{
     private static final long serialVersionUID = 1L;   
     private String user = "Username";
     private String pass = "Password";
     private String login = "Login";
     private JLabel languageInfo = new JLabel();
+    
+	Connection con;
+	java.sql.Statement st;
+	ResultSet rs;
 
     private JLabel info = new JLabel("Super Shopping Surfer");
     private JTextField userText = new InputField(user, 20);
@@ -27,7 +37,7 @@ public class LoginWindow extends JFrame implements ActionListener{
     JFrame frame = new JFrame("SSS");
     JPanel panel = new JPanel();
 
-    private String[] languages = {"Norwegian", "English"};
+    private String[] languages = {"Norwegian", "English", "Russian", "Chinese", "Spanish", "German", "French"};
     
 	private JComboBox comboBox = new JComboBox(languages);
 	
@@ -38,16 +48,15 @@ public class LoginWindow extends JFrame implements ActionListener{
 	public LoginWindow(){
 		
 		comboBox.setSelectedIndex(1);
-		comboBox.addActionListener(this);
-		
+		connect();
 		
         comboBox.addItemListener(
-                event -> {      																		// Listens to dropdownbox with languages
+                event -> {      																		// Listener for dropdownbox with languages
                     if (event.getSource() == comboBox) {
                     	JComboBox cb = (JComboBox)event.getSource();
                     	String msg = (String)cb.getSelectedItem();
                     	switch (msg) {
-							case "Norwegian": 				 	// system.setLanguage = norwegian - Select language Norwegian
+							case "Norwegian": 				 		// system.setLanguage = norwegian - Select language Norwegian
 								setUser("Brukernavn");
 								setPass("Passord");
 								setLogin("Logg inn");
@@ -59,6 +68,26 @@ public class LoginWindow extends JFrame implements ActionListener{
 								setPass("Password");
 								setLogin("Login");
 								languageInfo.setText("English selected");
+								break;
+								
+							case "Russian":
+								languageInfo.setText("Russian selected");
+								break;
+								
+							case "Chinese":
+								languageInfo.setText("Chinese selected");
+								break;
+								
+							case "Spanish":
+								languageInfo.setText("Spanish selected");
+								break;
+								
+							case "German":
+								languageInfo.setText("German selected");
+								break;
+								
+							case "French":
+								languageInfo.setText("French selected");
 								break;
 								
 	
@@ -91,40 +120,7 @@ public class LoginWindow extends JFrame implements ActionListener{
         
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
-	/*
-	public void actionPerformed(ActionEvent event){
-		                    if (event.getSource() == comboBox) {
-                    	JComboBox cb = (JComboBox)event.getSource();
-                    	String msg = (String)cb.getSelectedItem();
-                    	switch (msg) {
-							case "Norwegian": 				 	// system.setLanguage = norwegian - Select language Norwegian
-								setUser("Brukernavn");
-								setPass("Passord");
-								setLogin("Logg inn");
-								languageInfo.setText("Norwegian selected");
-								break;
-                    	
-							case "English": 						//system.setLanguage = english	- Select language english
-								setUser("Username");
-								setPass("Password");
-								setLogin("Login");
-								languageInfo.setText("English selected");
-								break;
-								
 	
-							default:
-								setUser("Username");
-								setPass("Password");
-								setLogin("Login");															// No language selected
-								languageInfo.setText("No language selected");
-								break;
-								
-                    	}
-		                    }
-	}*/
-	
-	
-    
     //Gui-helpclasses
     private class TopText extends JPanel{
     	public TopText(){
@@ -157,12 +153,46 @@ public class LoginWindow extends JFrame implements ActionListener{
 
     private class Buttonlistener implements ActionListener {			// Buttonlistener-login
         public void actionPerformed(ActionEvent event){
-            JButton button = (JButton) event.getSource();
-            if(button == loginButton){
-                														// Insert login-functionality
-            }
+            try {
+	            //JButton button = (JButton) event.getSource();
+	            
+	            String user = userText.getText().trim();
+	            String pass = passwordText.getText().trim();
+	            
+	            String sql = "select user, pass from _Tablename_ where user = '"+user+"'and pass = '"+pass+"' ";
+				rs = ((java.sql.Statement) st).executeQuery(sql);
+					
+	            int count = 0;
+	            while(rs.next()){
+	            	count++;
+	            }
+	            if (count == 1) {
+					JOptionPane.showMessageDialog(null, "User found, Access Granted!");
+				}else if (count > 1) {
+					JOptionPane.showMessageDialog(null, "Duplicate User, Access Denied");
+				}else {
+					JOptionPane.showMessageDialog(null, "User not Found");
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
         }
     }
+    
+    public void connect() {											// Connecting to database (Missing driver- and database address)
+		try {
+			String driver = "x.x.x.X";
+			getClass();
+			Class.forName(driver);
+			
+			String db = "x:x:x";
+			con = DriverManager.getConnection(db);
+			st = con.createStatement();
+			
+		} catch(Exception e){
+			}
+		}
+    
     public static void main(String[] args){								// Main-method, just runs the code
     	SwingUtilities.invokeLater(new Runnable() {
 			
@@ -173,12 +203,5 @@ public class LoginWindow extends JFrame implements ActionListener{
 			}
 		});
     }
-    
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-	
 }
 
