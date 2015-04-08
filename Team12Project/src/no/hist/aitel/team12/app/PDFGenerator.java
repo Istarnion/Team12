1e41c18e0c4b804fd3a9c648aba6fa73e079c9f7
andrbo <andreasborsheim91@gmail.com>
2015-04-08 16:30:57 +0200

    Implemented the use of the iText library to generate a .pdf-file, with a temporary image with our logo

--- Team12Project/src/no/hist/aitel/team12/app/PDFGenerator.java
+++ Team12Project/src/no/hist/aitel/team12/app/PDFGenerator.java
@@ -1,5 +1,39 @@
 package no.hist.aitel.team12.app;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import java.io.FileOutputStream;
+
+
 public class PDFGenerator {
 
+	public static void main (String[]args){
+		
+		Document document = new Document();
+		document.setPageSize(PageSize.A4);
+		
+		try{
+		PdfWriter.getInstance(document, new FileOutputStream("budgetdoc.pdf"));
+		
+		document.open();
+		//Watermark
+		Image image1 = Image.getInstance("Resources/images/watermark1.png");
+		image1.scaleToFit(PageSize.A4);
+		document.add(image1);
+		// adds report generated in Report class 
+		Paragraph paragraph = new Paragraph();
+		paragraph.add("This is a test");
+		document.add(paragraph);
+		document.close();
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+	
+	
+	}
+	
 }
