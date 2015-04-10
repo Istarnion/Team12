6ffeb4ef440da859a51d1079d81f0190551c636a
helkana <jorgen.skogstad@gmail.com>
2015-04-10 18:54:43 +0200

    Changed some layout on Overview, also added JTree

--- Team12Project/src/no/hist/aitel/team12/gui/OverviewTab.java
+++ Team12Project/src/no/hist/aitel/team12/gui/OverviewTab.java
@@ -1,8 +1,14 @@
 package no.hist.aitel.team12.gui;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 
 import javax.swing.JPanel;
+import javax.swing.JTree;
+import javax.swing.border.LineBorder;
+
+import no.hist.aitel.team12.app.Business;
 
 public class OverviewTab extends JPanel {
 
@@ -12,13 +18,25 @@ public class OverviewTab extends JPanel {
 	
 	private JPanel rightPanel;
 	
+	private JTree businessList;
+	
+	private Business[] businessArray;
+	
 	public OverviewTab() {
 		
 		this.setLayout(new BorderLayout());
 		
+		
+		
 		leftPanel = new JPanel(new BorderLayout());
 		rightPanel = new JPanel(new BorderLayout());
+		businessList = new JTree();
+		leftPanel.setPreferredSize(new Dimension(250, 0));		
+		businessList.setBorder(new LineBorder(Color.black));
+		
 		add(leftPanel, BorderLayout.WEST);
-		add(rightPanel, BorderLayout.EAST);
+		add(rightPanel, BorderLayout.CENTER);
+		
+		leftPanel.add(businessList, BorderLayout.CENTER);
 	}
 }
