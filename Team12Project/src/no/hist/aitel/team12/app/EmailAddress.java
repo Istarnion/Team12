4ee2bedffb608779a6eca05279d6a9173765053d
Roger Schei Bergheim <roger.schei.bergheim@live.no>
2015-04-10 12:50:00 +0200

    filled in the EmailAddress class with a validation-method and testclass

--- Team12Project/src/no/hist/aitel/team12/app/EmailAddress.java
+++ Team12Project/src/no/hist/aitel/team12/app/EmailAddress.java
@@ -1,14 +1,46 @@
 package no.hist.aitel.team12.app;
 
-public class EmailAddress {
-	public String emailAddress;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
 
-	public EmailAddress() {
-	}
+/**
+ * This is the EmailAdress class.
+ * 
+ * The method validateEmailAddress checks if the selected email-address is valid on the format xx@yy.com or xx.yy@xx.com
+ * 
+ * 
+ * @author Roger
+ *
+ */
+
+public class EmailAddress {
+	private String emailAddress;
 	
 	
-	public void validateEmailAddress(){				// Check if the email-address is in the format xx@yy.com
+	public static boolean isValidEmailAddress(String emailAddress){
+		boolean result = true;
+		try {
+			InternetAddress emailAddr = new InternetAddress(emailAddress);
+			emailAddr.validate();
+			
+		} catch (AddressException e) {
+			// TODO: handle exception
+			result = false;
+			System.out.println("IllegalArgumentException");
+		} 
+		return result;
 		
 	}
+
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+	
 
 }
