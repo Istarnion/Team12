e7c0c76348210ab9cecb495125059832e30eeaee
Hallgeir Løkken <istarnion@gmail.com>
2015-04-14 11:22:39 +0200

    Implemented single statement execution in the sqlTab

--- Team12Project/src/no/hist/aitel/team12/gui/SqlTab.java
+++ Team12Project/src/no/hist/aitel/team12/gui/SqlTab.java
@@ -21,31 +21,31 @@ public class SqlTab extends SSSTab {
 	private static final long serialVersionUID = 6417537010099126929L;
 
 	private SqlTextArea sqlArea;
-	
+
 	private JTable resultTable;
-	
+
 	private JTextArea outputArea;
-	
+
 	private JButton executeButton;
-	
+
 	private int rows, cols;
-	
+
 	private ListenerGroup listener;
-	
+
 	private final static String
-		EXECUTE_CMD = "EXECUTE",
-		EXECUTE_ALL = "EXECUTEALL";
-	
+	EXECUTE_CMD = "EXECUTE",
+	EXECUTE_ALL = "EXECUTEALL";
+
 	public SqlTab() {
 		rows = 20;
 		cols = 50;
-		
+
 		listener = new ListenerGroup();
-		
+
 		this.setLayout(new GridBagLayout());
-		
+
 		GridBagConstraints gbc = new GridBagConstraints();
-		
+
 		gbc.fill = GridBagConstraints.BOTH;
 		gbc.weightx = 0.5;
 		gbc.weighty = 0.9;
@@ -56,7 +56,7 @@ public class SqlTab extends SSSTab {
 		gbc.gridwidth = 3;
 		gbc.gridheight = 1;
 		this.add(sqlArea.generateScrollPane(), gbc);
-		
+
 		resultTable = new JTable();
 		JScrollPane resultTablePane = new JScrollPane(resultTable);
 		resultTablePane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
@@ -65,7 +65,7 @@ public class SqlTab extends SSSTab {
 		gbc.gridwidth = 3;
 		gbc.gridheight = 1;
 		this.add(resultTablePane, gbc);
-		
+
 		gbc.fill = GridBagConstraints.HORIZONTAL;
 		gbc.weighty = 0.1;
 		gbc.weightx = 0.9;
@@ -76,7 +76,7 @@ public class SqlTab extends SSSTab {
 		gbc.gridwidth = 5;
 		gbc.gridheight = 1;
 		this.add(outputArea, gbc);
-		
+
 		gbc.weightx = 0.1;
 		gbc.fill = GridBagConstraints.NONE;
 		executeButton = new JButton(Text.getString("exe"));
@@ -91,23 +91,38 @@ public class SqlTab extends SSSTab {
 
 	void executeBulk() {
 		String[] statements = sqlArea.getText().split(";");
-		
+
 		for(String statement : statements) {
 			executeStatement(statement.trim());
 		}
 	}
-	
+
 	void executeSingleStatement() {
-		System.out.println("Not yet implemented");
+		// assuming the editorText is the contents of the edit window
+		String[] statements = sqlArea.getText().split(";");
+
+		int statementCharactersProcessed = 0;
+
+		// assuming caret is at position caretIndex
+
+		int statementIndex = 0;
+		String statement = null;
+		int caretIndex = sqlArea.getCaretPosition();
+		while (caretIndex>statementCharactersProcessed+1) {
+		    // re-add the missing ;
+		    statement = statements[statementIndex++] + ";";
+		    statementCharactersProcessed += statement.length();
+		}
+		executeStatement(statement.trim());
 	}
-	
+
 	private void executeStatement(String statement) {
 		if (statement == null) return;
-		
+
 		if(statement.isEmpty()) return;
-		
+
 		String actionType = statement.split(" ")[0].toLowerCase();
-		
+
 		if(actionType.startsWith("select")) {
 			String[][] result = DatabaseFactory.getDatabase().executeQuery(statement);
 			if(result.length == 1) {
@@ -115,28 +130,28 @@ public class SqlTab extends SSSTab {
 			}
 			else {
 				outputArea.setText("");
-		
+
 				String[][] content = new String[result.length-1][result[0].length];
 				for(int row=0; row<content.length; row++) {
 					for(int col=0; col<content[0].length; col++) {
 						content[row][col] = result[row+1][col];
 					}
 				}
-				
+
 				DefaultTableModel tableModel = new DefaultTableModel(content, result[0]);
 				resultTable.setModel(tableModel);
 			}
 		}
 		else if(actionType.startsWith("insert") || actionType.startsWith("update") || actionType.startsWith("delete")) {
 			String result = DatabaseFactory.getDatabase().executeUpdate(statement);
-			
+
 			outputArea.setText(result);
 		}
 		else {
 			System.out.println("Invalid SQL action type: "+actionType);
 		}
 	}
-	
+
 	private class ListenerGroup implements ActionListener, KeyListener {
 
 		@Override
@@ -166,19 +181,19 @@ public class SqlTab extends SSSTab {
 		@Override
 		public void actionPerformed(ActionEvent ae) {
 			switch(ae.getActionCommand()) {
-				case EXECUTE_ALL:
-				{
-					executeBulk();
-				} break;
-				
-				case EXECUTE_CMD:
-				{
-					executeSingleStatement();
-				} break;
-				
-				default:
-					System.out.println("Invalid action command given in the sql tab.");
-					break;
+			case EXECUTE_ALL:
+			{
+				executeBulk();
+			} break;
+
+			case EXECUTE_CMD:
+			{
+				executeSingleStatement();
+			} break;
+
+			default:
+				System.out.println("Invalid action command given in the sql tab.");
+				break;
 			}
 		}
 	}
@@ -186,6 +201,6 @@ public class SqlTab extends SSSTab {
 	@Override
 	public void refresh() {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
