9294d5269babc592b27613948fcf67b90d83adf6
Roger Schei Bergheim <roger.schei.bergheim@live.no>
2015-04-20 12:25:57 +0200

    Added edit-functionality on user parameters

--- Team12Project/src/no/hist/aitel/team12/gui/UserTab.java
+++ Team12Project/src/no/hist/aitel/team12/gui/UserTab.java
@@ -15,7 +15,6 @@ import javax.swing.table.DefaultTableModel;
 import no.hist.aitel.team12.database.*;
 import no.hist.aitel.team12.util.Text;
 
-//import javax.swing.JList;
 /**
  * 
  * @author Roger
@@ -26,10 +25,11 @@ public class UserTab extends SSSTab {
 
 	private static final long serialVersionUID = -445246322816259272L;
 	
-	private JButton newUser = new JButton(Text.getString("newuser"));	
-	private JButton editUser = new JButton(Text.getString("edituser"));
+	JButton newUser = new JButton(Text.getString("newuser"));	
+	JButton editUser = new JButton(Text.getString("edituser"));
 	//private JList<?> centerList;
-	JLabel header = new JLabel("Username");
+	String headertext = "Username";
+	JLabel header = new JLabel(headertext);
 	
 	InputField firstName = new InputField(Text.getString("firstname"), 20);
 	InputField lastName = new InputField(Text.getString("lastname"), 20);
@@ -39,14 +39,25 @@ public class UserTab extends SSSTab {
 	InputField telephone = new InputField(Text.getString("tel"), 12);
 	InputField salary = new InputField(Text.getString("sal"), 20);
 	InputField username = new InputField(Text.getString("usr"), 20);
-	InputField password = new InputField(Text.getString("pwd"), 20);
+	PasswordInputField password = new PasswordInputField(Text.getString("pwd"), 20);
 	JButton createUser = new JButton(Text.getString("createuser"));
 	
-	private JPanel buttonPanel;
-
-	private JTable resultTable;
+	JButton editFirstName = new JButton(Text.getString("edit"));
+	JButton editLastName = new JButton(Text.getString("edit"));
+	JButton editAddress = new JButton(Text.getString("edit"));
+	JButton editZipCode = new JButton(Text.getString("edit"));
+	JButton editEmail = new JButton(Text.getString("edit"));
+	JButton editTelephone = new JButton(Text.getString("edit"));
+	JButton editSalary = new JButton(Text.getString("edit"));
+	JButton editUsername = new JButton(Text.getString("edit"));
+	JButton editPassword = new JButton(Text.getString("edit"));
 	
+	private JPanel buttonPanel;
 	private JPanel mainPanel;
+	JPanel editPanel;
+	JPanel bottomPanel;
+	
+	private JTable resultTable;
 
 	public UserTab() {
 		this.setLayout(new BorderLayout());
@@ -55,7 +66,6 @@ public class UserTab extends SSSTab {
 		mainPanel.setLayout(new BorderLayout());
 		
 		JPanel topPanel = new JPanel();
-		//header = new JLabel(databaseConnection.getUserID());
 		topPanel.add(header);
 		mainPanel.add(topPanel, BorderLayout.NORTH);
 
@@ -84,15 +94,83 @@ public class UserTab extends SSSTab {
 		buttonPanel = new JPanel();
 		buttonPanel.setLayout(new GridLayout(1,2));
 		buttonPanel.add(newUser);		
-		buttonPanel.add(editUser);
-		ButtonListener buttonListener = new ButtonListener();
-		editUser.addActionListener(buttonListener);
-		newUser.addActionListener(buttonListener);
+		buttonPanel.add(editUser);	
+		
 		userPanel.add(buttonPanel, BorderLayout.SOUTH);
 		
 		this.add(userPanel, BorderLayout.WEST);
+		
+		ButtonListener buttonListener = new ButtonListener();
+		editUser.addActionListener(buttonListener);
+		newUser.addActionListener(buttonListener);
+		editFirstName.addActionListener(buttonListener);
+		editLastName.addActionListener(buttonListener);
+		editAddress.addActionListener(buttonListener);
+		editZipCode.addActionListener(buttonListener);
+		editEmail.addActionListener(buttonListener);
+		editTelephone.addActionListener(buttonListener);
+		editSalary.addActionListener(buttonListener);
+		editUsername.addActionListener(buttonListener);
+		editPassword.addActionListener(buttonListener);	
+		
+	}
+	/*
+	public void updateFields(){
+		newUser.setText(Text.getString("newuser"));
+		editUser.setText(Text.getString("edituser"));
+		
+		header.setText("test");
+		
+		firstName.setDefaultText(Text.getString("firstname"));
+		lastName.setDefaultText(Text.getString("lastname"));
+		address.setDefaultText(Text.getString("adr"));
+		zipCode.setDefaultText(Text.getString("zip"));
+		electronicmail.setDefaultText(Text.getString("email"));
+		telephone.setDefaultText(Text.getString("tel"));
+		salary.setDefaultText(Text.getString("sal"));
+		username.setDefaultText(Text.getString("usr"));
+		password.setDefaultText(Text.getString("pwd"));
+		
+		createUser.setText("createuser");
+		
+		editFirstName.setText(Text.getString("edit"));
+		editLastName.setText(Text.getString("edit"));
+		editAddress.setText(Text.getString("edit"));
+		editZipCode.setText(Text.getString("edit"));
+		editEmail.setText(Text.getString("edit"));
+		editTelephone.setText(Text.getString("edit"));
+		editSalary.setText(Text.getString("edit"));
+		editUsername.setText(Text.getString("edit"));
+		editPassword.setText(Text.getString("edit"));
+		
+		newUser.repaint();
+		editUser.repaint();
+		header.repaint();
+		firstName.repaint();
+		lastName.repaint();
+		address.repaint();
+		zipCode.repaint();
+		electronicmail.repaint();
+		telephone.repaint();
+		salary.repaint();
+		username.repaint();
+		password.repaint();
+		
+		createUser.repaint();
+		
+		editFirstName.repaint();
+		editLastName.repaint();
+		editAddress.repaint();
+		editZipCode.repaint();
+		editEmail.repaint();
+		editTelephone.repaint();
+		editSalary.repaint();
+		editUser.repaint();
+		editPassword.repaint();
+		
 	}
 	
+	*/
 	public void setUserTable(DatabaseConnection databaseConnection, int userID){
 		
 		String statement = databaseConnection.getUserStatement(userID);
@@ -139,14 +217,14 @@ public class UserTab extends SSSTab {
 				DatabaseFactory.setup();
 				db = DatabaseFactory.getDatabase();
 				
-		/* ----------------------- Show the save-button for the saving a new user ----------*/
+		/* ----------------------- Show the save-button for saving a new user ----------*/
 				JPanel bottomPanel = new JPanel();
 				bottomPanel.add(createUser);
 				mainPanel.add(bottomPanel, BorderLayout.SOUTH);
 				
 				try {
 					
-					 @SuppressWarnings("unused")
+					 @SuppressWarnings({ "unused", "deprecation" })
 					Object newUser = db.createUser(
 							firstName.getText(), 
 							lastName.getText(), 
@@ -169,10 +247,64 @@ public class UserTab extends SSSTab {
 				
 				
 			}else if (button == editUser) {
-		/* ------------------------------Edit existing user--------------------------------- */ 
+		/* ------------------------------Edit existing user--------------------------------- */
+				
+				JPanel editPanel = new JPanel();
+				editPanel.setLayout(new GridLayout(11,1));
+				editPanel.add(editFirstName);
+				editPanel.add(editLastName);
+				editPanel.add(editAddress);
+				editPanel.add(editZipCode);
+				editPanel.add(editEmail);
+				editPanel.add(editTelephone);
+				editPanel.add(editSalary);
+				editPanel.add(editUsername);
+				editPanel.add(editPassword);
+				mainPanel.add(editPanel, BorderLayout.EAST);
+				
+				JButton b = new JButton();
+				
+				if (b == editFirstName) {
+					/*		change text "edit to "save"		*/
+					/*		Save new first name				*/
+					editFirstName = new JButton(Text.getString("save"));
+					//firstName = new firstName();
+					
+				}else if (b == editLastName) {
+					/*		change text "edit to "save"		*/
+					/*		Save new last name				*/
+					editLastName = new JButton(Text.getString("save"));
+				}else if (b == editAddress) {
+					/*		change text "edit to "save"		*/
+					/*		Save new address				*/
+					editAddress = new JButton(Text.getString("save"));
+				}else if (b == editZipCode) {
+					/*		change text "edit to "save"		*/
+					/*		Save new zip code				*/
+					editZipCode = new JButton(Text.getString("save"));
+				}else if (b == editEmail) {
+					/*		change text "edit to "save"		*/
+					/*		Save new email					*/
+					editEmail = new JButton(Text.getString("save"));
+				}else if (b == editTelephone) {
+					/*		change text "edit to "save"		*/
+					/*		Save new telephone				*/
+					editTelephone = new JButton(Text.getString("save"));
+				}else if (b == editSalary) {
+					/*		change text "edit to "save"		*/
+					/*		Save new salary					*/
+					editSalary = new JButton(Text.getString("save"));
+				}else if (b == editUsername) {
+					/*		change text "edit to "save"		*/
+					/*		Save new username				*/
+					editUsername = new JButton(Text.getString("save"));
+				}else if (b == editPassword) {
+					/*		change text "edit to "save"		*/
+					/*		Save new password				*/
+					editPassword = new JButton(Text.getString("save"));
+				}
 				
 			}
-			
 		}
 		
 	}
@@ -183,3 +315,4 @@ public class UserTab extends SSSTab {
 		
 	}
 }
+
