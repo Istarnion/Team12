751754cdcde4f731e2f2b8cd0b481e6bb3292ef1
Helblidi <gjermund.jevne@gmail.com>
2015-04-20 17:13:33 +0200

    Changed all set-methods in business to String

--- Team12Project/src/no/hist/aitel/team12/app/Address.java
+++ Team12Project/src/no/hist/aitel/team12/app/Address.java
@@ -27,7 +27,7 @@ public class Address {
 		return address;
 	}
 
-	public void setAdress(String address) {
+	public void setAddress(String address) {
 		this.address = address;
 	}
 
--- Team12Project/src/no/hist/aitel/team12/app/Business.java
+++ Team12Project/src/no/hist/aitel/team12/app/Business.java
@@ -1,5 +1,6 @@
 package no.hist.aitel.team12.app;
 
+import java.sql.PreparedStatement;
 import java.util.ArrayList;
 
 import no.hist.aitel.team12.database.Database;
@@ -9,29 +10,30 @@ import no.hist.aitel.team12.util.DoubleMetaphoneUtils;
 public class Business {
 
 	public final int businessId;
-	
+
 	private String businessName;
-	
+
 	private String description;
-	
+
 	private EmailAddress email;
-	
+
 	private Address address;
-	
+
 	private int telephone;
-	
+
 	private int openingHours;
-	
+
 	private ArrayList<Revenue> revenue;
-	
+
 	private Database db;
+
 
 	public Business(
 			int businessId, String businessName,
 			EmailAddress email, int telephone,
 			int openingHours, String description, 
 			Address address, ArrayList<Revenue> revenue) {
-		
+
 		this.businessId 	= businessId;
 		this.businessName 	= businessName;
 		this.email 			= email;
@@ -40,63 +42,85 @@ public class Business {
 		this.description 	= description;
 		this.address 		= address;
 		this.revenue 		= revenue;
-		db 					= DatabaseFactory.getDatabase();
-		}
+	}
 
 	public String getBusinessnameDmp() {
 		return DoubleMetaphoneUtils.encodeString(businessName);
 	}
-	
+
 	public String getBusinessName() {
 		return businessName;
 	}
 
-	public void setBusinessName(String businessName) {
-		this.businessName = businessName;
+	public boolean setBusinessName(String businessName) {
+		if(db.updateBusiness(businessId, "business_name", businessName)) {
+			this.businessName = businessName;
+			return true;
+		}
+		return false;
 	}
 
 	public EmailAddress getEmail() {
 		return email;
 	}
 
-	public void setEmail(EmailAddress email) {
-		this.email = email;
+	public boolean setEmail(String email) {
+		return true;
 	}
 
 	public int getTelephone() {
 		return telephone;
 	}
 
-	public void setTelephone(int telephone) {
-		this.telephone = telephone;
+	public boolean setTelephone(String telephone) {
+		try {
+			int parsedTlf = Integer.parseInt(telephone);
+			if(String.valueOf(parsedTlf).length() != 8){
+				return false;
+			}
+			if(db.updateBusiness(businessId, "telephone", telephone)){
+				this.telephone = parsedTlf;
+				return true;
+			}
+		}
+		catch (NumberFormatException e) {
+			return false;
+		}
+		return false;
 	}
 
 	public int getOpeningHours() {
 		return openingHours;
 	}
 
-	public void setOpeningHours(int openingHours) {
-		this.openingHours = openingHours;
+	public boolean setOpeningHours(String openingHours) {
+		return true;
 	}
 
 	public int getBusinessId() {
 		return businessId;
 	}
-	
+
 	public String getDescription() {
 		return description;
 	}
 
-	public void setDescription(String description) {
+	public boolean setDescription(String description) {
 		this.description = description;
+		return true;
 	}
-	
+
 	public Address getAddress() {
 		return address;
 	}
 
-	public void setAddress(Address address) {
-		this.address = address;
+	public boolean setAddress(String address) {
+		this.address.setAddress(address);
+		return true;
+	}
+
+	public boolean setZipcode(String zipcode) {
+		return true;
 	}
 
 	public ArrayList<Revenue> getRevenue() {
