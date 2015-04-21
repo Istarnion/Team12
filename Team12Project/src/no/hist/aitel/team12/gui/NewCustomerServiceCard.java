7e45db23641556cfe7a6634cc813a3fd5c59c7c9
Roger Schei Bergheim <roger.schei.bergheim@live.no>
2015-04-21 17:49:46 +0200

    Added errorchecking and setup for functionality on user adding

--- Team12Project/src/no/hist/aitel/team12/gui/NewCustomerServiceCard.java
+++ Team12Project/src/no/hist/aitel/team12/gui/NewCustomerServiceCard.java
@@ -7,10 +7,13 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
+import no.hist.aitel.team12.app.EmailAddress;
+import no.hist.aitel.team12.app.User;
 import no.hist.aitel.team12.util.Text;
 
 public class NewCustomerServiceCard extends JPanel{
@@ -21,6 +24,8 @@ public class NewCustomerServiceCard extends JPanel{
 	private static final long serialVersionUID = -1259252191776134538L;
 	private JButton saveButton, cancelButton;
 	private JPanel buttonPanel, fieldPanel, labelPanel;
+	
+	private User user;
 	
 	private JTextField
 		firstName, lastName, username, email, personalAddress, personalZip, telephone, salary;
@@ -69,6 +74,94 @@ public class NewCustomerServiceCard extends JPanel{
 		saveButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent ae) {
+				StringBuilder errMsg = new StringBuilder();
+				int errCount = 0;
+				
+				/* CHECKING FIELDS */
+				if(firstName.getText().length() > 30) {
+					errCount++;
+					errMsg.append("-First name is too long. Max thirty characters.\n");
+				}
+				
+				if(lastName.getText().length() > 30) {
+					errCount++;
+					errMsg.append("-Last name is too long. Max thirty characters.\n");
+				}
+				
+				if(personalAddress.getText().length() > 30) {
+					errCount++;
+					errMsg.append("-Address field is too long. Max 30 characters.\n");
+				}
+				
+				try {
+					Integer.parseInt(personalZip.getText());
+					if(personalZip.getText().length() > 4) {
+						errCount++;
+						errMsg.append("-Zipcode must be four digits long.\n");
+					}
+				}
+				catch(NumberFormatException e) {
+					errCount++;
+					errMsg.append("-Zip code can only be numbers, and four digits long.\n");
+				}
+				
+				if(!EmailAddress.isValidEmailAddress(email.getText())) {
+					errCount++;
+					errMsg.append("-Email address is invalid.\n");
+				}
+				
+				try {
+					Integer.parseInt(telephone.getText());
+					if(telephone.getText().length() > 8) {
+						errCount++;
+						errMsg.append("-Telephone number must be eight digits long.\n");
+					}
+				}
+				catch(NumberFormatException e) {
+					errCount++;
+					errMsg.append("-Telephone number must be all numbers, and eight digits long.\n");
+				}
+				
+				try {
+					Integer.parseInt(salary.getText());
+				}
+				catch(NumberFormatException e) {
+					errCount++;
+					errMsg.append("-Salary must be all numbers.\n");
+				}
+				/* DONE CHECKING FIELDS */
+				
+				if(errCount > 0) {
+					if(errCount == 1) {
+						JOptionPane.showMessageDialog(
+								null,
+								"There was an error in your input:\n"+errMsg.toString(),
+								Text.getString("err"),
+								JOptionPane.ERROR_MESSAGE);
+					}
+					else {
+						JOptionPane.showMessageDialog(
+								null,
+								"There was an error in your input:\n"+errMsg.toString(),
+								Text.getString("err"),
+								JOptionPane.ERROR_MESSAGE);
+					}
+					
+					return;
+				}
+				/*
+				if(user.updateData(
+						firstName.getText(), lastName.getText(),
+						personalAddress.getText(), Integer.parseInt(personalZip.getText()),
+						new EmailAddress(email.getText()), Integer.parseInt(telephone.getText()),
+						Integer.parseInt(salary.getText()))) {
+					
+					updateCard(user);
+				}
+				else {
+					JOptionPane.showMessageDialog(null, Text.getString("dbErr"), Text.getString("err"), JOptionPane.ERROR_MESSAGE);
+				}
+				*/
 				
 			}
 		});
@@ -76,7 +169,7 @@ public class NewCustomerServiceCard extends JPanel{
 		cancelButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent ae) {
-				
+				if(user != null) updateCard(user);
 			}
 		});
 	}
@@ -90,6 +183,22 @@ public class NewCustomerServiceCard extends JPanel{
 		personalZip.setText("");
 		telephone.setText("");
 		salary.setText("");
+	}
+	
+	public void updateCard(User u) {
+		user = u;
+		
+		if(u == null) return;
+		
+		firstName.setText(u.getFirstName()!=null?u.getFirstName():"");
+		lastName.setText(u.getLastName()!=null?u.getLastName():"");
+		username.setText(u.getUsername()!=null?u.getUsername():"");
+		email.setText(u.getEmail().getEmailAddress()!=null?u.getEmail().getEmailAddress():"");
+		personalAddress.setText(u.getAddress().getAdress()!=null?u.getAddress().getAdress():"");
+		personalZip.setText(u.getAddress().getZipcode()+""!=null?u.getAddress().getZipcode()+"":"");
+		telephone.setText(u.getTelephone()+""!=null?u.getTelephone()+"":"");
+		salary.setText(u.getSalary()+""!=null?u.getSalary()+"":"");
+		
 	}
 
 	
--- Team12Project/src/no/hist/aitel/team12/gui/NewPersonnelCard.java
+++ Team12Project/src/no/hist/aitel/team12/gui/NewPersonnelCard.java
@@ -8,12 +8,15 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
 import com.sun.org.apache.xpath.internal.operations.String;
 
+import no.hist.aitel.team12.app.EmailAddress;
+import no.hist.aitel.team12.app.User;
 import no.hist.aitel.team12.util.Text;
 
 public class NewPersonnelCard extends JPanel{
@@ -26,6 +29,8 @@ public class NewPersonnelCard extends JPanel{
 
 	private JButton saveButton, cancelButton;
 	private JPanel buttonPanel, fieldPanel, labelPanel;
+	
+	private User user;
 	
 	private JTextField
 		firstName, lastName, email, personalAddress, personalZip, telephone, salary;
@@ -82,6 +87,94 @@ public class NewPersonnelCard extends JPanel{
 		saveButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent ae) {
+				StringBuilder errMsg = new StringBuilder();
+				int errCount = 0;
+				
+				/* CHECKING FIELDS */
+				if(firstName.getText().length() > 30) {
+					errCount++;
+					errMsg.append("-First name is too long. Max thirty characters.\n");
+				}
+				
+				if(lastName.getText().length() > 30) {
+					errCount++;
+					errMsg.append("-Last name is too long. Max thirty characters.\n");
+				}
+				
+				if(personalAddress.getText().length() > 30) {
+					errCount++;
+					errMsg.append("-Address field is too long. Max 30 characters.\n");
+				}
+				
+				try {
+					Integer.parseInt(personalZip.getText());
+					if(personalZip.getText().length() > 4) {
+						errCount++;
+						errMsg.append("-Zipcode must be four digits long.\n");
+					}
+				}
+				catch(NumberFormatException e) {
+					errCount++;
+					errMsg.append("-Zip code can only be numbers, and four digits long.\n");
+				}
+				
+				if(!EmailAddress.isValidEmailAddress(email.getText())) {
+					errCount++;
+					errMsg.append("-Email address is invalid.\n");
+				}
+				
+				try {
+					Integer.parseInt(telephone.getText());
+					if(telephone.getText().length() > 8) {
+						errCount++;
+						errMsg.append("-Telephone number must be eight digits long.\n");
+					}
+				}
+				catch(NumberFormatException e) {
+					errCount++;
+					errMsg.append("-Telephone number must be all numbers, and eight digits long.\n");
+				}
+				
+				try {
+					Integer.parseInt(salary.getText());
+				}
+				catch(NumberFormatException e) {
+					errCount++;
+					errMsg.append("-Salary must be all numbers.\n");
+				}
+				/* DONE CHECKING FIELDS */
+				
+				if(errCount > 0) {
+					if(errCount == 1) {
+						JOptionPane.showMessageDialog(
+								null,
+								"There was an error in your input:\n"+errMsg.toString(),
+								Text.getString("err"),
+								JOptionPane.ERROR_MESSAGE);
+					}
+					else {
+						JOptionPane.showMessageDialog(
+								null,
+								"There was an error in your input:\n"+errMsg.toString(),
+								Text.getString("err"),
+								JOptionPane.ERROR_MESSAGE);
+					}
+					
+					return;
+				}
+				/*
+				if(user.updateData(
+						firstName.getText(), lastName.getText(),
+						personalAddress.getText(), Integer.parseInt(personalZip.getText()),
+						new EmailAddress(email.getText()), Integer.parseInt(telephone.getText()),
+						Integer.parseInt(salary.getText()))) {
+					
+					updateCard(user);
+				}
+				else {
+					JOptionPane.showMessageDialog(null, Text.getString("dbErr"), Text.getString("err"), JOptionPane.ERROR_MESSAGE);
+				}
+				*/
 				
 			}
 		});
@@ -89,6 +182,7 @@ public class NewPersonnelCard extends JPanel{
 		cancelButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent ae) {
+				if(user != null) updateCard(user);
 			}
 		});
 	}
@@ -101,6 +195,21 @@ public class NewPersonnelCard extends JPanel{
 		personalZip.setText("");
 		telephone.setText("");
 		salary.setText("");
+	}
+	
+	public void updateCard(User u) {
+		user = u;
+		
+		if(u == null) return;
+		
+		firstName.setText(u.getFirstName()!=null?u.getFirstName():"");
+		lastName.setText(u.getLastName()!=null?u.getLastName():"");
+		email.setText(u.getEmail().getEmailAddress()!=null?u.getEmail().getEmailAddress():"");
+		personalAddress.setText(u.getAddress().getAdress()!=null?u.getAddress().getAdress():"");
+		personalZip.setText(u.getAddress().getZipcode()+""!=null?u.getAddress().getZipcode()+"":"");
+		telephone.setText(u.getTelephone()+""!=null?u.getTelephone()+"":"");
+		salary.setText(u.getSalary()+""!=null?u.getSalary()+"":"");
+		
 	}
 
 }
